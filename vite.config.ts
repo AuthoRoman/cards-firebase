e142e9e226a23116c72de6ec27f27f2060beb86c
AuthoRoman <kit.kiram2017@yandex.ru>
2024-07-09 06:20:45 +0300

    fixBuggh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/cards-firebase",
   plugins: [vue()],
+  base: "/cards-firebase",
 });
